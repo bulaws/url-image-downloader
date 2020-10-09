af45f009ae97e15769f19767610a91f0793224c9
Tarambulavs <tarambulavs@gmail.com>
2020-10-09 13:23:50 +0300

    Clear not need file and composer update

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__."/vendor/autoload.php";
-
-use Image\Downloader\ImageDownload;
-
-$imageDownload = new ImageDownload();
-
-$imageDownload->imageDownloadByUrl('https://stackoverflow.com/questions/38760592/how-to-get-response-body-in-guzzle', 'image', 'test.jpg');
