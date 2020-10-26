6df4360c6b4f35abd256ec48ab6635f3ea2b4f61
bulaws <tarambulavs@gmail.com>
2020-10-26 22:08:30 +0200

    Modified image download test folder

--- tests/ImageDownloadTest.php
+++ tests/ImageDownloadTest.php
@@ -16,16 +16,16 @@ class ImageDownloadTest extends TestCase
         $imageValidator = new ImageValidator();
 
         $downloader = new ImageDownload($imageValidator);
-        \mkdir('tests/image', 0777, true);
+        \mkdir('imageTest', 0777, true);
         try {
-            $downloader->imageDownloadByUrl('https://24tv.ua/resources/photos/news/1200x675_DIR/201908/1195520.jpg?201908174431', 'tests/image', 'field.jpg');
+            $downloader->imageDownloadByUrl('https://24tv.ua/resources/photos/news/1200x675_DIR/201908/1195520.jpg?201908174431', 'imageTest', 'field.jpg');
         } catch (UrlImageDownLoadException $e) {
             throw new $e;
         }
-        $file = \file_exists('tests/image/field.jpg');
+        $file = \file_exists('imageTest/field.jpg');
         $this->assertTrue($file);
-        \unlink('tests/image/field.jpg');
-        \rmdir('tests/image');
+        \unlink('imageTest/field.jpg');
+        \rmdir('imageTest');
     }
 
     /**
@@ -37,10 +37,10 @@ class ImageDownloadTest extends TestCase
     {
         $imageValidator = new ImageValidator();
         $downloader = new ImageDownload($imageValidator);
-        \mkdir('tests/image', 0777, true);
-        $downloader->imageDownloadByUrl('https://www.php.net/manual/ru/function.file-exists.php', 'tests/image', 'field.php');
-        $file = \file_exists('tests/image/field.php');
+        \mkdir('imageTest', 0777, true);
+        $downloader->imageDownloadByUrl('https://www.php.net/manual/ru/function.file-exists.php', 'imageTest', 'field.php');
+        $file = \file_exists('imageTest/field.php');
         $this->assertFalse($file);
-        \rmdir('tests/image');
+        \rmdir('imageTest');
     }
 }
